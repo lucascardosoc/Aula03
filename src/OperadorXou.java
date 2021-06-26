67b548203133391ccb477d70b8f47943e9f75224
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-06-25 21:15:04 -0300

    Implementando o operador Xou

--- src/OperadorXou.java
+++ src/OperadorXou.java
@@ -13,6 +13,13 @@ public class OperadorXou {
 		temperatura = leitor.nextDouble();
 		
 		//Aqui é preciso criar um if que verifique as duas condições para que o os ventiladores sejam ligados ou não
+		if (umidade>70 ^ temperatura > 26) {
+			System.out.println("Ligando os ventiladores");
+		}else {
+			System.out.println("Por favor, entrar em contato com um agrônomo");
+		}
+		
+		
 		
 		leitor.close();
 		
